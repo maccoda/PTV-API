4a343b067f8ccacb1ef686574e7979f69dab7a6f
Dylan Maccora <maccora17@gmail.com>
2017-09-28 21:53:12 +1000

    Moved test resources to json files.

--- src/test/java/ptvobjects/PtvLineTest.java
+++ src/test/java/ptvobjects/PtvLineTest.java
@@ -1,45 +1,23 @@
 package ptvobjects;
 
 import com.google.gson.Gson;
-import org.junit.*;
+import org.junit.Test;
+import util.TestUtils;
+
+import java.io.FileReader;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PtvLineTest {
 
-    static String testString;
-
-    /**
-     * Create mock JSON object.
-     *
-     * @throws Exception
-     *         fucked up
-     */
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-        testString = "{\"route_type\": 0, \"line_id\": 6, "
-                + "\"line_name\": \"Frankston\", \"line_number\": \"Frankston\", "
-                + "\"line_name_short\": \"Frankston\", \"line_number_long\": \"\"}";
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
 
     @Test
     public void testPopulateFields() throws Exception {
         final Gson gson = new Gson();
 
-        final PtvLine line = gson.fromJson(testString, PtvLine.class);
+        final String resourcePath = TestUtils.getResourcePath("testLine.json");
+        final PtvLine line = gson.fromJson(new FileReader(resourcePath), PtvLine.class);
 
         assertTrue(line.getRouteType().equals(PtvRouteType.Train));
         assertEquals(6, line.getLineId());
