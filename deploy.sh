81d90d325b8e45690d833e5e2809ebb9d39e10fe
Dylan Maccora <maccora17@gmail.com>
2017-09-30 11:44:02 +1000

    Using env instead of property.

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ if [ -z ${TRAVIS_TAG} ]; then
     echo "Not a tag. Will not deploy"
 else
     echo "Deploying ${TRAVIS_TAG}"
-    ./gradlew bintrayUpload -PtravisTag=${TRAVIS_TAG}
+    ./gradlew bintrayUpload
 fi
