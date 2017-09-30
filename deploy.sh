93ad6e797c51af564108310872d378ad833c6207
Dylan Maccora <maccora17@gmail.com>
2017-09-30 11:41:05 +1000

    Got some deployment.

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -z ${TRAVIS_TAG} ]; then
+    echo "Not a tag. Will not deploy"
+else
+    echo "Deploying ${TRAVIS_TAG}"
+    ./gradlew bintrayUpload -PtravisTag=${TRAVIS_TAG}
+fi
