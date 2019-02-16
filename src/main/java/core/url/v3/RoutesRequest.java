8e77c6239c75d7bbf213939752af3eb98c45f802
Dylan Maccora <maccora17@gmail.com>
2019-02-17 08:21:19 +1100

    Add routeId and builder to RoutesRequest

--- src/main/java/core/url/v3/RoutesRequest.java
+++ src/main/java/core/url/v3/RoutesRequest.java
@@ -1,11 +1,38 @@
 package core.url.v3;
 
 public class RoutesRequest implements Request {
+
+    private final Integer routeId;
+
+    RoutesRequest(final Integer routeId) {
+        this.routeId = routeId;
+    }
+
     @Override
     public String toUrl() {
         final UrlPathBuilder builder = new UrlPathBuilder();
         builder.appendPathSegment("v3");
         builder.appendPathSegment("routes");
+        if (routeId != null) {
+            builder.appendPathSegment(routeId);
+        }
         return builder.build();
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Integer routeId;
+
+        Builder withRouteId(final Integer id) {
+            routeId = id;
+            return this;
+        }
+
+        public RoutesRequest build() {
+            return new RoutesRequest(routeId);
+        }
     }
 }
--- src/test/java/core/url/v3/RoutesRequestTest.java
+++ src/test/java/core/url/v3/RoutesRequestTest.java
@@ -8,10 +8,19 @@ public class RoutesRequestTest {
 
     @Test
     public void shouldBuildCorrectUrl() {
-        final RoutesRequest request = new RoutesRequest();
+        final RoutesRequest request = RoutesRequest.builder().build();
 
         final String url = request.toUrl();
 
         assertEquals("/v3/routes", url);
+    }
+
+    @Test
+    public void shouldBuildUrlWithRouteId() {
+        final RoutesRequest request = RoutesRequest.builder().withRouteId(10).build();
+
+        final String url = request.toUrl();
+
+        assertEquals("/v3/routes/10", url);
     }
 }
