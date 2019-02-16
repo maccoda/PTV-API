11c60ec24e8ea5102c747e51dcc751c0852b499e
Dylan Maccora <maccora17@gmail.com>
2019-02-16 12:35:54 +1100

    Change print to log

--- src/main/java/deserialize/ResponseDeserializerWrapper.java
+++ src/main/java/deserialize/ResponseDeserializerWrapper.java
@@ -3,21 +3,24 @@ package deserialize;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
+import java.util.logging.Logger;
+
 public class ResponseDeserializerWrapper {
+    private final static Logger logger = Logger.getLogger(ResponseDeserializerWrapper.class.getSimpleName());
     private final Gson gson;
 
-    public ResponseDeserializerWrapper(Gson gson) {
+    public ResponseDeserializerWrapper(final Gson gson) {
         this.gson = gson;
     }
 
-    public <T> T deserialize(String response, ResponseDeserializer<T> deserializer) {
+    public <T> T deserialize(final String response, final ResponseDeserializer<T> deserializer) {
         validateStatus(response);
         return deserializer.deserialize(response);
     }
 
-    private void validateStatus(String response) {
-        JsonObject obj = gson.fromJson(response, JsonObject.class);
-        System.out.println(obj.toString());
+    private void validateStatus(final String response) {
+        final JsonObject obj = gson.fromJson(response, JsonObject.class);
+        logger.info(obj.toString());
         if (obj.get("status").getAsJsonObject().get("health").getAsInt() != 1) {
             throw new RuntimeException("Status is not OK");
         }
